cbb5bd903bdb9fdc6527973d27eae3408cbe2c8b
Nick Gray <nicholasnbg@gmail.com>
2020-07-28 23:10:33 +1000

    Match store logic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-import puppeteer from 'puppeteer';
+import { getFromMatchStore, genKey } from './matchStore';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import { scrapePage } from './scraping';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const main = async (): Promise<void> => {
+  const maybeNewPost = await scrapePage();
+
+  if (maybeNewPost) {
+    const matchKey = genKey(maybeNewPost.minSince, Date.now());
+    const alreadySeen = getFromMatchStore(matchKey);
+    if (!alreadySeen) {
+      //generate notification and then save in match store
+    }
+  }
+};
+
+main();
